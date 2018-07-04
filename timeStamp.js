8089b62fa993d9bb34f87a79ad6d3b2e1bdfd7df
json <760809318@qq.com>
2018-07-04 22:40:58 +0800

    提交 - 时间戳转换为时间函数

---
+++ timeStamp.js
@@ -0,0 +1,13 @@
+
+// 将时间戳转换为日期时间
+
+function timeStamp(timeStr) {
+  var date = new Date(timeStr * 1000); //时间戳如果是10位 *1000, 如果是13位不用 * 1000
+  Y = date.getFullYear() + '-';
+  M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+  D = date.getDate() + ' ';
+  h = date.getHours() + ':';
+  m = date.getMinutes() + ':';
+  s = date.getSeconds();
+  return Y + M + D + h + m + s;
+}
